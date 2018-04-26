4a6fe9a6c95db4b63cdc52c97fe696491d6f1652
Liam-Cafearo <lcafearo@gmail.com>
2018-04-26 07:10:26 +0100

    Updated lesson files

--- mysql_with_python/database/mysql.py
+++ mysql_with_python/database/mysql.py
@@ -10,21 +10,33 @@ class MySQLDatabase(object):
     and a destructor method that will close the database
     connection once the driver object is destroyed.
     """
-
-    def __init__(self, database_name, username,
-                password, host='localhost'):
+    def __init__(self, database_name, username, password, host='localhost'):
         """
         Here we'll try and connect to the database
         using the variables we passed through
         and if the connection fails we'll print out the error
         """
         try:
-            self.db = _mysql.connect(db=database_name, host=host,
-                                    user=username, passwd=password)
+            self.db = _mysql.connect(db=database_name, host=host, user=username, passwd=password)
             self.database_name = database_name
             print "Connected to MySQL!"
         except _mysql.Error, e:
             print e
+    
+    def get_availble_tables(self):
+        """
+        This method will allow us to see what
+        tables are available to us when we're
+        running our queries
+        """
+        cursor = self.db.cursor()
+        cursor.execute("SHOW TABLES;")
+
+        self.tables = cursor.fetechall()
+
+        cursor.close()
+
+        return self.tables
     
     def __del__(self):
         """
