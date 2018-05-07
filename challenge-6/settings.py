e0f0adc89952dd661ed01a79058f03bb00abe8ed
Liam-Cafearo <lcafearo@gmail.com>
2018-05-07 19:47:45 +0100

    Added more challenge files

---
+++ challenge-6/settings.py
@@ -0,0 +1,10 @@
+"""
+These are the settings we'll use in order
+to connect to our database
+"""
+db_config = {
+    'db_name': 'my_db',
+    'host': 'localhost',
+    'user': 'root',
+    'pass': 'b5DEg4dnQH9T'
+}
